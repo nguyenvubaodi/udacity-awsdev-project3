0bf5033428b67a885588d6da9049d5d8f97d9f80
nguyenvubaodi <nguyenvubaodi@gmail.com>
2024-08-18 07:11:56 +0900

    Project completed

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0ebbaca7689849499448cb925a60bc2-1495202126.ap-northeast-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
